2571ba58496fb82d9b948e1dde24a36a968bbc03
h4x4d <sidenkoleg@gmail.com>
2024-03-21 17:12:18 +0300

    change header opening time

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -2,7 +2,7 @@
 
 addEventListener("scroll", () => {
     const yPos = document.documentElement.scrollTop || document.body.scrollTop;
-    if (yPos + 100 >= window.screen.height) {
+    if (yPos + 100 >= window.innerHeight) {
         document.querySelector('.header').classList.add('header_active')
     } else {
         document.querySelector('.header').classList.remove('header_active')
