0b64d150358a78f3c71df7339a3f2c5c80f2a191
h4x4d <sidenkoleg@gmail.com>
2024-07-28 21:04:25 +0300

    refactor

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -7,29 +7,29 @@ const codeAlert = document.querySelector('.code__alert')
 
 
 codeForm.addEventListener('submit', async function (event) {
-    event.preventDefault();
-    let code = codeInput.value;
-    codeButton.disabled = codeInput.disabled = true
+        event.preventDefault();
+        let code = codeInput.value;
+        codeButton.disabled = codeInput.disabled = true
 
 
-    codeAlert.classList.remove('code__alert_type_error')
-    codeAlert.innerText = "creating image..."
-    codeAlert.classList.add('code__alert_type_wait')
+        codeAlert.classList.remove('code__alert_type_error')
+        codeAlert.innerText = "creating image..."
+        codeAlert.classList.add('code__alert_type_wait')
 
-    let response = await fetch("https://ths-gallery.ru/deckview/?code=" + code)
+        let response = await fetch("https://ths-gallery.ru/deckview/?code=" + code)
 
-    if (response.ok) {
-        let result = await response.blob()
-        const urlCreator = window.URL || window.webkitURL;
+        if (response.ok) {
+            let result = await response.blob()
+            const urlCreator = window.URL || window.webkitURL;
 
-        codeImage.src = urlCreator.createObjectURL(result);
-        codeImage.classList.remove('code__image_not_shown')
-        codeAlert.classList.remove('code__alert_type_wait')
-    } else {
-        codeAlert.classList.remove('code__alert_type_wait')
-        codeAlert.innerText = "error occurred while creating"
-        codeAlert.classList.add('code__alert_type_error')
+            codeImage.src = urlCreator.createObjectURL(result);
+            codeImage.classList.remove('code__image_not_shown')
+            codeAlert.classList.remove('code__alert_type_wait')
+        } else {
+            codeAlert.classList.remove('code__alert_type_wait')
+            codeAlert.innerText = "error occurred while creating"
+            codeAlert.classList.add('code__alert_type_error')
+        }
+        codeButton.disabled = codeInput.disabled = false
     }
-    codeButton.disabled = codeInput.disabled = false
-}
 )
